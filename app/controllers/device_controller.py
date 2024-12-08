3047f00a918f52265398f9e9027e528cb365d85d
Achmad Baihaqi <haqiachd.me@gmail.com>
2024-12-08 14:38:17 +0700

    feat : update device controller

--- app/controllers/device_controller.py
+++ app/controllers/device_controller.py
@@ -1,24 +1,26 @@
-from flask import Blueprint, jsonify, request
-from ..utils.response_util import ResponseUtil
-from firebase_admin import storage, firestore
-from firebase_admin.firestore import SERVER_TIMESTAMP
-import uuid
-import time
-import string
-import random
-import time
 import os
-from werkzeug.utils import secure_filename
-from inference_sdk import InferenceHTTPClient
-from PIL import Image, ImageDraw, ImageFont
-import warnings
-import pandas as pd
-import cv2
-import mahotas as mt
-warnings.filterwarnings("ignore", category=UserWarning, module="PIL")
-import joblib
-import shutil
 import json
+import time
+import uuid
+import shutil
+import random
+import string
+import warnings
+import joblib
+import pandas as pd
+import mahotas as mt
+import cv2
+from datetime import datetime, timedelta
+from threading import Thread
+from flask import Blueprint, jsonify, request
+from werkzeug.utils import secure_filename
+from PIL import Image, ImageDraw, ImageFont
+from firebase_admin import storage, firestore, messaging
+from firebase_admin.firestore import SERVER_TIMESTAMP
+import pytz
+from inference_sdk import InferenceHTTPClient
+warnings.filterwarnings("ignore", category=UserWarning, module="PIL")
+from ..utils.response_util import ResponseUtil
 
 device_bp = Blueprint('device', __name__)
 client = firestore.client()
@@ -51,7 +53,6 @@ def create_device():
 
     try:
         client.collection('devices').document(str(device_id)).set(device)
-
         return ResponseUtil.success("Device created successfully", data=token)
 
     except Exception as e:
@@ -143,6 +144,21 @@ def update_sensors():
             "sensors": sensors,
             "updatedAt": SERVER_TIMESTAMP
         })
+
+        # Get the actual data of the device document
+        device_data = device_doc.get().to_dict()
+
+        # Data untuk notifikasi
+        send_notif = {
+            "deviceName": device_data['name'],
+            'waterVol': water_vol,
+            'token': token,
+            'title': 'Volume air tinggal ' + str(water_vol) + '%',
+            'body': 'Segera lakukan pengisian air agar penyiraman dapat berlanjut'
+        }
+
+        response = send_notifications_util(token, send_notif)
+        print('response : ', response)
 
         return ResponseUtil.success("Sensors updated successfully", data=None)
     except Exception as e:
@@ -346,7 +362,10 @@ def add_photo_by_token():
 
         # Mengunggah foto dan mendapatkan URL
         photoUrl, predictions = upload_photo_to_storage(photo, device_id)
-        predictions = json.loads(predictions)
+
+        print('predict : ', predictions)
+        print('predict : ', photoUrl)
+
         if photoUrl:
             new_photo = {
                 "createdAt": SERVER_TIMESTAMP,
@@ -672,3 +691,140 @@ def detect(image_input, output_image, output_folder="output_crops", output_folde
     print(f"Annotated image with bounding boxes and labels saved at: {output_image}")
 
     return predictions
+
+def send_notifications_util(token, notif_data):
+
+    if not token:
+        return jsonify({"error": "Token is required"}), 400
+
+    if not notif_data or not isinstance(notif_data, dict):
+        return jsonify({"error": "Notif Data must be a valid dictionary"}), 400
+
+    if 'title' not in notif_data or 'body' not in notif_data:
+        return jsonify({"error": "Notif Data must include 'title' and 'body'"}), 400
+
+    try:
+        # Ambil device berdasarkan token
+        device_query = client.collection('devices').where('token', '==', token).limit(1).stream()
+        device_id = None
+
+        for device in device_query:
+            device_id = device.id
+            break
+
+        if not device_id:
+            return jsonify({"error": "Device not found for the provided token"}), 404
+    
+        print('device id : ', device_id)
+
+        # Query users who might have the device in their 'devices' field
+        user_refs = client.collection('users').stream()
+
+        fcm_tokens = []
+
+        jakarta_tz = pytz.timezone('Asia/Jakarta')
+        now = datetime.now(jakarta_tz)
+
+        # Iterate through each user document
+        for user_ref in user_refs:
+            user_data = user_ref.to_dict()
+            fcm_token = user_data.get('fcmToken')
+            device_ids = user_data.get('devices', {}).keys()
+            
+            if device_id in device_ids:
+
+                # If FCM token exists, proceed to check the last notification time
+                if fcm_token:
+                    # Get the most recent notification for the user (ordered by sendAt)
+                    user_doc_ref = client.collection('users').document(user_ref.id)
+                    notifications_ref = user_doc_ref.collection('notifications').order_by('sendAt', direction=firestore.Query.DESCENDING).limit(1)
+
+                    notifications = notifications_ref.stream()
+                    is_empty = not any(notifications)
+
+                    if not is_empty:
+                        latest_notification = notifications_ref.stream()
+
+                        last_sent_time = None
+                        for notification in latest_notification:
+                            notification_data = notification.to_dict()
+                            last_sent_time = notification_data.get('sendAt')
+
+                        # If there is a last sent time, compare it to the current time
+                        if last_sent_time:
+                            # Convert last_sent_time (which is in UTC) to Asia/Jakarta timezone
+                            last_sent_time = last_sent_time.astimezone(jakarta_tz)  # Convert to Asia/Jakarta timezone
+
+                            # Calculate the time difference
+                            time_diff = now - last_sent_time
+
+                            # Round to seconds (ignores microseconds)
+                            time_diff_seconds = time_diff.total_seconds()
+
+                            # Check if time difference is less than 1 hour (3600 seconds)
+                            if time_diff_seconds < 3600:
+                                continue  # Skip sending notification if less than 1 hour has passed
+                            else:
+                                # If we get here, it means the notification can be sent
+                                fcm_tokens.append(fcm_token)
+                    else:
+                        fcm_tokens.append(fcm_token)
+
+        if not fcm_tokens:
+            return jsonify({"success": "No users found with the specified token"}), 200
+
+        # Send notifications to the collected FCM tokens
+        fcm_tokens = list(set(fcm_tokens))
+        for fcm_token in fcm_tokens:
+            message = messaging.Message(
+                notification=messaging.Notification(
+                    title=notif_data['title'],
+                    body=notif_data['body']
+                ),
+                token=fcm_token
+            )
+            messaging.send(message)
+
+            user_ref =  client.collection('users').where('fcmToken', '==', fcm_token).limit(1).get()
+            
+            if user_ref:
+                # Check if user_ref contains any results
+                if user_ref:
+                    user_doc_ref = user_ref[0].reference  # Get the document reference of the first result
+                    history_ref = user_doc_ref.collection('notifications')  # Access the user's 'notifications' subcollection
+
+                    # Add the new notification to the history
+                    notif_data['sendAt'] = SERVER_TIMESTAMP
+                    history_ref.add(notif_data)
+            else:
+                print('user not found')
+
+        return jsonify({"message": "Notifications sent successfully!"}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Failed to send notifications: {str(e)}"}), 500
+
+
+@device_bp.route('/notif/send', methods=['POST'])
+def send_notifications():
+    data = request.json
+    token = data.get('token')
+
+    # Pastikan semua parameter yang diperlukan tersedia
+    if not token:
+        return jsonify({"error": "Token parameter is required"}), 400
+
+    # Data notifikasi yang akan dikirim
+    send_notif = {
+        "deviceName": 'Testing',
+        'waterVol': '1000',
+        'token': token,
+        'title': 'Testing Notif',
+        'body': 'Developer Mode'
+    }
+
+    # Panggil utilitas pengiriman notifikasi
+    response, status_code = send_notifications_util(token, send_notif)
+
+    # Kembalikan response secara langsung dengan status code yang sesuai
+    return response, status_code
